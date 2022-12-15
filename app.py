fa1d58a4b22032105024806fe930ca40bb75120c
jst0312 <120348528+jst0312@users.noreply.github.com>
2022-12-15 15:43:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
-def
 
-for i in range(len(address)):
-    a = address[i].split(' ')
-    address[i]= " ".join(a[0:4])
-print(address)
+def main():
+    pass
+
+
+
+
+
+
+menu =['']
